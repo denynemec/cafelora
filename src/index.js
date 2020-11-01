f51dde407762f0480325db375f7eddeb3243221c
Daniel Němec <nemecd@cngroup.dk>
2020-11-01 11:10:41 +0100

    Navigation works.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,21 @@
 import './index.html';
 import './style.css';
 
-console.log('funguju!');
+// 3
+const navigationHandlers = () => {
+  const navBtn = document.getElementById('nav-btn');
+
+  const navigationLinks = document.querySelectorAll('a');
+
+  const navElement = document.getElementById('nav');
+
+  const navElmentToggle = () => navElement.classList.toggle('nav-closed');
+
+  navBtn.addEventListener('click', navElmentToggle);
+
+  navigationLinks.forEach((navLink) => {
+    navLink.addEventListener('click', navElmentToggle);
+  });
+};
+
+navigationHandlers();
